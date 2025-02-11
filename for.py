05c58b27969a3461608eaecae084c0a3f2b1fa24
ANVARJONOV NURMUHAMMAD <shahlohonteshaboyov@gmail.com>
2025-02-11 11:19:36 +0500

    Create for.py

---
+++ for.py
@@ -0,0 +1,25 @@
+#1-m
+# a = ("olma","sabzi","piyoz","kartoshka","bexi")
+# n=0
+# for son in a :
+#     n+=1
+#     print(son)
+# print(f"Qiymat {n} marta takrorlandi ")
+# 2-m
+# for son in range(11,100,2):
+#     print(f"{son**3}")
+# 3-m
+# son =[]
+# for s in range(5):
+#     s = input(" Sevimli kino nomini kiriting")
+#     son.append(s)
+#print(son)
+# 4-m
+# n =int(input("Bugun necha kishi bilan suhbat qurdingiz>> "))
+# a=1
+# b=[]
+# for son in range(n):
+#     c =input(f"{a}-odamni kiriting:")
+#     b.append(c)
+#     a +=1
+# print(b)
